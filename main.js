3f3639f7a19f9689385fcac83a1df31a3426c7ad
sezzh <sezzhltd@gmail.com>
2017-09-25 01:26:19 -0500

    working drag and drop API example

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 (function () {
-  console.log('working clousure');
+  var draggableElement = document.querySelector('[data-drag="button"]')
+
+  var dropZoneElement = document.querySelector('[data-drag="drop-zone"]')
+
+  draggableElement.addEventListener('dragstart', function (event) {
+    event.dataTransfer.setData('text/plain', 'HFDSASCGF')
+    event.dataTransfer.dropEffect = 'copy'
+  })
+
+  dropZoneElement.addEventListener('drop', function (event) {
+    event.preventDefault()
+    console.dir(event);
+    dropZoneElement.style.backgroundColor = 'green'
+    dropZoneElement.innerHTML = event.dataTransfer.getData('text/plain')
+  })
+
+  dropZoneElement.addEventListener('dragover', function (event) {
+    event.preventDefault()
+    console.log('dragover event!');
+    dropZoneElement.style.backgroundColor = 'olive'
+  })
+
+  draggableElement.addEventListener('dragend', function (event) {
+    event.preventDefault()
+    dropZoneElement.style.backgroundColor = 'teal'
+  })
 })()
